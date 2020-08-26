27c468fb7fc497fd536439ec90ca8f94b958d336
emre kayacan <emre.kayacan@msn.com>
2020-08-27 01:42:38 +0300

    created init edit data example

---
+++ src/constants/initData.ts
@@ -0,0 +1,9 @@
+import { ITransaction } from "interfaces/transaction";
+export const initEditData: ITransaction = {
+  id: -10,
+  name: "",
+  description: "",
+  amount: -10,
+  date: "",
+  currency: "",
+};
